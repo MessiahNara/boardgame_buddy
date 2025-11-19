952d261a74f629545c71dea4cb65a8af9d8869ab
Jasper Ferrer <jasperianferrer25@gmail.com>
2025-11-20 00:17:49 +0800

    Update Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'my_collection.dart';
+import 'player_finder.dart';
 
 class HomePage extends StatelessWidget {
-final String username;
-const HomePage(this.username);
+  final String username;
+  const HomePage(this.username);
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: Center(
-child: Text(
-"Welcome, $username!",
-style: const TextStyle(fontSize: 24),
-),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff1B1C1E),
+      appBar: AppBar(
+        title: Text("Welcome, $username"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => MyCollectionPage(username),
+                  ),
+                );
+              },
+              child: const Text("My Collection"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => PlayerFinderPage()),
+                );
+              },
+              child: const Text("Player Finder"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
